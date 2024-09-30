3e8c59b976425b67bce700f2891a5fa20ba7497d
KEHINDE ADIGUN <keahnney01@gmail.com>
2024-09-30 13:47:07 +0100

    rebase done

--- components/dash/BookingTable.tsx
+++ components/dash/BookingTable.tsx
@@ -1,6 +1,7 @@
 import { BookingType, BookingColumns } from "@/components/Columns";
 import { DataTable } from "@/components/DataTable";
 import Link from "next/link";
+import { BookingOptions } from "../SortData";
 
 async function getData(): Promise<BookingType[]> {
 	return [
@@ -56,29 +57,57 @@ async function getData(): Promise<BookingType[]> {
 	];
 }
 
-export default async function BookingTable({ isHome }) {
-	const data = await getData();
+const getSelect = [
+  {
+    name: "Booking Type",
+    options: ["Instant Booking", "Scheduled Booking"],
+  },
+  {
+    name: "Status",
+    options: ["Pending", "In Progress", "Completed", "Cancelled"],
+  },
+  {
+    name: "Service Type",
+    options: [
+      "Driving",
+      "Indoor Cleaning",
+      "Solar Installation",
+      "Electrical Cleaning",
+    ],
+  },
+  {
+    name: "Role",
+    options: ["Customer", "Talent"],
+  },
+];
+
+
+
+export default async function BookingTable({ isHome , data }: {isHome: boolean, data: BookingType []}) {
+	// const data = await getData();
 
 	return (
-
-		<div className="card my-5">
+    <div className="card my-5">
 			<DataTable
-				columns={BookingColumns}
-				data={data}
-				title="Bookings"
-			/>
+				// @ts-ignore
+        columns={BookingColumns}
+        data={data}
+        title="Bookings"
+        selectOptions={BookingOptions}
+				path='/bookings'
+				filterType={getSelect}
+      />
 
-			{isHome && (
-				<div className=" my-8 flex justify-center items-center">
-					<Link
-						href="/bookings"
-						className="bg-primaryBlue px-5 py-3 text-white rounded-lg"
-					>
-						View All Bookings
-					</Link>
-				</div>
-			)}
-		</div>
-	);
-
+      {isHome && (
+        <div className=" my-8 flex justify-center items-center">
+          <Link
+            href="/bookings"
+            className="bg-primaryBlue px-5 py-3 text-white rounded-lg"
+          >
+            View All Bookings
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
